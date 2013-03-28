f141d9b23bbfaee09aaee5acc5fa8c852c016ee6
MBQsniper <MBQsniper@hotmail.com>
2013-03-28 02:51:23 -0700

    Add some devices

---
+++ Project-M/device/common/libbt/vnd_buildcfg.mk
@@ -0,0 +1,20 @@
+intermediates := $(local-intermediates-dir)
+
+ifneq ($(BOARD_BLUEDROID_VENDOR_CONF),)
+SRC := $(BOARD_BLUEDROID_VENDOR_CONF)
+else
+SRC := $(call my-dir)/include/$(addprefix vnd_, $(addsuffix .txt,$(basename $(TARGET_DEVICE))))
+ifeq (,$(wildcard $(SRC)))
+# configuration file does not exist. Use default one
+SRC := $(call my-dir)/include/vnd_generic.txt
+endif
+endif
+GEN := $(intermediates)/vnd_buildcfg.h
+TOOL := $(TOP_DIR)external/bluetooth/bluedroid/tools/gen-buildcfg.sh
+
+$(GEN): PRIVATE_PATH := $(call my-dir)
+$(GEN): PRIVATE_CUSTOM_TOOL = $(TOOL) $< $@
+$(GEN): $(SRC)  $(TOOL)
+	$(transform-generated-source)
+
+LOCAL_GENERATED_SOURCES += $(GEN)
